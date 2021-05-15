71f72f47a11b4efbcb3ea309b0fd35da301e7ab7
Qois-69912 <qois.firosi@gmail.com>
2021-05-15 16:52:07 +0700

    Menambahkan config baru & memperbaiki script
    - Menambahkan config yang terkait dengan redshift.
    - Beberapa config yang terletak dalam suatu subdir, Jadi harus
      membuat subdir tersebut sebelum bisa dicopy.
    - Beberapa config file harus direname menjadi '.'file.

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pac_package=""
 yay_package=""
+target=""
 
 check_dependencies() {
     if ! command -v git "$1"; then 
@@ -12,14 +13,12 @@ check_dependencies() {
 install_dependencies() {
     if ! [ -z "${pac_package}" ]; then
         echo "Installing all the dependencies (require root)"
-        echo "pac"
-        # sudo pacman $pac_package
+        sudo pacman $pac_package
     fi
 
     if ! [ -z "${yay_package}" ]; then
         echo "Installing all the dependencies (require root) [aur]"
-        echo "yay"
-        # yay -S $yay_package
+        yay -S $yay_package
     fi
 
     if [[ -z "${pac_package}" && -z "${yay_package}" ]]; then
@@ -29,7 +28,7 @@ install_dependencies() {
 
 installasi_config() {
     # Catatan : Jangan check apakah file ada atau tidak, sebab
-    # bila file ada, maka tidak bisa di rewrite dengan config baru
+    # bila file ada, maka tidak bisa men-rewrite config lama dgn yang baru
 
     echo "Installasi config $1"
     
@@ -47,13 +46,30 @@ installasi_config() {
         check_dependencies git 
 
     # alacritty
-    elif $1 == "alacritty.yml" &> /dev/null ; then
+    elif $1 == "alacritty.yml" &> /dev/null; then
         check_dependencies alacritty
 
+    # redshift
+    elif $1 == "redshift.conf" &> /dev/null; then
+        check_dependencies redshift
     fi
 
-    # Nanti diganti dengan curl (sementara)
-    cp ../dotfiles/$1 $2 &> /dev/null
+    # Target dimana config akan disimpan
+    target="$2"
+
+    # Membuat dir, kalau belum ada
+    if [[ $3 == "dir" ]]; then
+        if ! [[ -d $2 ]]; then
+            mkdir $2
+        fi
+
+    # Merename file, menjadi '.'file
+    elif [[ $3 == "dot" ]]; then
+        target="$2/.$1"
+    fi
+
+    # Nanti 'dotfiles/$1' diganti dengan '/tmp/'
+    cp dotfiles/$1 $target
 }
 
 main() {
@@ -61,9 +77,11 @@ main() {
     echo "Warning!!, ini akan menyebabkan configurasi yang sekarang terhapus"
 
     # Installasi config
-    installasi_config bashrc $HOME
-    installasi_config gitconfig $HOME
-    installasi_config alacritty.yml $HOME/.config
+    installasi_config bashrc $HOME dot
+    installasi_config gitconfig $HOME dot
+    installasi_config alacritty.yml $HOME/.config/alacritty dir
+    installasi_config redshift.conf $HOME/.config
+    installasi_config xprofile $HOME dot
 
     # Installasi dependencies
     install_dependencies
