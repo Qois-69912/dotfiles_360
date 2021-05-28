582154246793d5865c63e6224f81df897c673735
Qois-69912 <qois.firosi@gmail.com>
2021-05-28 08:22:05 +0700

    Installasi program hanya akan dari "package.sh"

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,30 +1,5 @@
 #!/bin/sh
-# pac_package=""
-# yay_package=""
 target=""
-
-# check_dependencies() {
-    # if [ "$(command -v "$1")" = "" ]; then 
-        # echo "Tidak ada program $1, nanti akan diinstall"
-        # pac_package="$pac_package $1"
-    # fi
-# }
-
-# install_dependencies() {
-    # if ! [ -z "${pac_package}" ]; then
-        # echo "Installing all the dependencies (require root)"
-        # sudo pacman -S $pac_package
-    # fi
-
-    # if ! [ -z "${yay_package}" ]; then
-        # echo "Installing all the dependencies (require root) [aur]"
-        # yay -S $yay_package
-    # fi
-
-    # if [ -z "${pac_package}" ] && [ -z "${yay_package}" ]; then
-        # echo "Tidak ada hal yang harus diinstall"
-    # fi
-# }
 
 installasi_config() {
     # Catatan : Jangan check apakah file ada atau tidak, sebab
@@ -41,18 +16,6 @@ installasi_config() {
             sudo chsh -s /bin/bash
             sudo ln -sf dash /bin/sh
         fi
-
-    # gitconfig
-    # elif [ "$1" = "gitconfig" ]; then
-        # check_dependencies git 
-
-    # alacritty
-    # elif [ "$1" = "alacritty.yml" ]; then
-        # check_dependencies alacritty
-
-    # redshift
-    # elif [ "$1" = "redshift.conf" ]; then
-        # check_dependencies redshift
     fi
 
     # Target dimana config akan disimpan
@@ -83,9 +46,6 @@ main() {
     installasi_config alacritty.yml $HOME/.config/alacritty dir
     installasi_config redshift.conf $HOME/.config
     installasi_config xprofile $HOME dot
-
-    # Installasi dependencies
-    install_dependencies
 }
 
 main
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 main() {
-    echo "Updating system"
-    yay -Syyu
+    echo "Updating system.."
+    yay -Syyu --noconfirm
 
     echo "Installing packages.."
     cat /tmp/dotfiles_360/list-packages.txt | awk '$3 == "p"' | awk '{print $1}' > /tmp/dotfiles_360/pacman
     sudo pacman -S --needed - < /tmp/dotfiles_360/pacman
 
     cat /tmp/dotfiles_360/list-packages.txt | awk '$3 == "a"' | awk '{print $1}' > /tmp/dotfiles_360/aur
-    yay -S --needed - < /tmp/dotfiles_360/aur
+    yay -S --needed --noconfirm - < /tmp/dotfiles_360/aur
 
-    "Removing packages..."
+    echo "Removing packages..."
     cat /tmp/dotfiles_360/list-packages.txt | awk '$3 == "r"' | awk '{print $1}' > /tmp/dotfiles_360/uninstall
     for x in $(cat /tmp/dotfiles_360/uninstall); do sudo pacman -Rns $x; done
 }
